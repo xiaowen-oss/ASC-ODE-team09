474e3115b167652e03d895b488c2db6ba5603593
Melis Saka <meliss00@unet.univie.ac.at>
2025-11-23 00:15:22 +0100

    add Improved Euler method, results files, and updated plot script

--- demos/plotmassspring.py
+++ demos/plotmassspring.py
@@ -1,24 +1,33 @@
 import numpy as np
-data = np.loadtxt('output_test_ode.txt', usecols=(0, 1, 2))
-# print (data)
-
 import matplotlib.pyplot as plt
 
-plt.plot(data[:,0], data[:,1], label='position')
-plt.plot(data[:,0], data[:,2], label='velocity')
+data_exp = np.loadtxt('compare_results/explicit_100.txt', usecols=(0, 1, 2))
+data_imp = np.loadtxt('compare_results/implicit_100.txt', usecols=(0, 1, 2))
+data_impv = np.loadtxt('compare_results/improved_100.txt', usecols=(0, 1, 2))
+
+
+plt.figure(figsize=(10,5))
+plt.plot(data_exp[:,0], data_exp[:,1], label='Explicit Euler - position')
+plt.plot(data_imp[:,0], data_imp[:,1], label='Implicit Euler - position')
+plt.plot(data_impv[:,0], data_impv[:,1], label='Improved Euler - position')
+
 plt.xlabel('time')
-plt.ylabel('value')
-plt.title('Mass-Spring System Time Evolution')
+plt.ylabel('position')
+plt.title('Mass-Spring System Position Over Time')
 plt.legend()
 plt.grid()
 plt.show()
 
+plt.figure(figsize=(10,5))
+plt.plot(data_exp[:,1], data_exp[:,2], label='Explicit Euler')
+plt.plot(data_imp[:,1], data_imp[:,2], label='Implicit Euler')
+plt.plot(data_impv[:,1], data_impv[:,2], label='Improved Euler')
 
-plt.plot(data[:,1], data[:,2], label='phase plot')
 plt.xlabel('position')
 plt.ylabel('velocity')
-plt.title('Mass-Spring System Phase Plot')
+plt.title('Phase Plot: Mass-Spring System')
 plt.legend()
 plt.grid()
 plt.show()
+
 
